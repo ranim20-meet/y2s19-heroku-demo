e32a35afb1ba8eea681ea1c85eb622faca865c7c
myname21-meet <myname21@meet.mit.edu>
2019-07-31 05:56:19 -0400

    Added flask templates code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+opposite_day = False
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
+def home_page():
+    foods = ["Sushi", "Pasta", "Steak", "Sweets"]
+    least_foods = ["Brussels sproute", "Onion", "Spicy things", "Ariel: fish", "Yuval K: Hummus"]
+    return render_template("index.html", foods = foods, least_foods = least_foods, opposite_day = opposite_day)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+   app.run(debug = True)
